d38c7ba09bfc7a995181df41dc9d0e25af4d9d50
Valentijn Nieman <valentijnnieman@gmail.com>
2017-11-03 17:41:21 +0100

    cleanup

--- audio_streamer_server/main.go
+++ audio_streamer_server/main.go
@@ -1,44 +1,42 @@
 package main
 
 import (
+	"encoding/binary"
 	"github.com/gordonklaus/portaudio"
-	//"time"
-  "net/http"
-  "encoding/binary"
-  "fmt"
+	"net/http"
 )
 
 func main() {
 
 	portaudio.Initialize()
 	defer portaudio.Terminate()
-  buffer := make([]float32, 44100)
-  stream, err := portaudio.OpenDefaultStream(1, 0, 44100, len(buffer), func(in []float32) {
-    //out = in
-    //buffer = in
-    for i := range buffer{
-      buffer[i] = in[i]
-    }
-  })
-  chk(err)
-  chk(stream.Start())
-  //chk(stream.Stop())
-  defer stream.Close()
+	buffer := make([]float32, 44100)
+	stream, err := portaudio.OpenDefaultStream(1, 0, 44100, len(buffer), func(in []float32) {
+		for i := range buffer {
+			buffer[i] = in[i]
+		}
+	})
+	chk(err)
 
-  http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Transfer-Encoding", "chunked")
-    w.Header().Set("Content-Type", "audio/wave")
-    for true {
-      if(len(buffer) == len(buffer)) {
-        fmt.Println("Sending binary!")
-        fmt.Println(buffer)
-        binary.Write(w, binary.BigEndian, &buffer)
-        return
-      }
-    }
-  })
+	http.HandleFunc("/start", func(w http.ResponseWriter, r *http.Request) {
+		chk(stream.Start())
+		defer stream.Close()
+	})
+	http.HandleFunc("/stop", func(w http.ResponseWriter, r *http.Request) {
+		chk(stream.Stop())
+	})
+	http.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Transfer-Encoding", "chunked")
+		w.Header().Set("Content-Type", "audio/wave")
+		for true {
+			if len(buffer) == len(buffer) {
+				binary.Write(w, binary.BigEndian, &buffer)
+				return
+			}
+		}
+	})
 
-  http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", nil)
 }
 
 func chk(err error) {
